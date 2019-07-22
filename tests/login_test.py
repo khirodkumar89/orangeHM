644994cf44efa1953b46aeda257abd7af2e49529
khirodkumar <khirod.kumar89@gmail.com>
2019-07-22 14:22:30 +0530

    tests

---
+++ tests/login_test.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import pytest
+
+class TestLogin():
+    @pytest.fixture(scope="class")
+    def test_setup(self):
+        global driver
+        driver = webdriver.Chrome(executable_path="C:\\Users\\khirod\\PycharmProjects\\orangeHM\\drivers\\chromedriver.exe")
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        yield
+        driver.close()
+        driver.quit()
+        print("Test completed")
+
+    def test_login(self, test_setup):
+
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        driver.find_element_by_id("txtUsername").send_keys("Admin")
+        driver.find_element_by_id("txtPassword").send_keys("admin123")
+        driver.find_element_by_id("btnLogin").click()
+
+    def test_logout(self, test_setup):
+        driver.find_element_by_id("welcome").click()
+        driver.find_element_by_link_text("Logout").click()
+
+
+
+
